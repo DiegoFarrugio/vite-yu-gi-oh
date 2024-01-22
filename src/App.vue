1bd732bf427cba801f203fa67807ea2d283468d1
DiegoFarrugio <147447339+DiegoFarrugio@users.noreply.github.com>
2024-01-22 15:54:07 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+export default {
+  data(){
+    return{
+
+    }
+  },
+
+  methods: {},
+
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+}
+
+</script>
+
+
+
+<template>
+  <AppHeader/>
+  <AppMain/>
+  <AppFooter/>
+</template>
+
+
+
+<style lang="scss" scoped>
+@use "./assets/scss/main.scss" as *;
+</style>
